364f47643491585c02e5bfa2647656371e4e12bf
Olga Kim <ol4ik.kim17@gmail.com>
2018-05-14 01:48:19 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+    Animal animal = new Animal();
+    animal.makeSound();
+    Cat cat = new Cat();
+    cat.makeSound();
+    Dog dog = new Dog();
+    dog.makeSound();
+    }
+}
